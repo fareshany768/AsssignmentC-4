41d5583a67f7eb64f3e7ba31a8a4df69c6de5003
fareshany <fareeeshany@gmail.com>
2024-12-18 13:12:42 -0800

    Solve Q1

--- AsssignmentC#4/Program.cs
+++ AsssignmentC#4/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace AsssignmentC_4
+﻿using System.Reflection.Metadata;
+using System.Reflection;
+using System.Threading.Channels;
+
+namespace AsssignmentC_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //Difference Between Passing Value Type Parameters by Value and by Reference
+           // By Value: A copy of the variable is passed to the function. Changes made to the parameter inside the function do not affect the original variable.
+           // By Reference: A reference to the original variable is passed.Changes made to the parameter inside the function affect the original variable.
+            #endregion
+
         }
+
     }
 }
